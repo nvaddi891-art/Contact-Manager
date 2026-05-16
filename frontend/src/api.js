17a057f34a299645a3c5b7a7ec27c0fb2a28c512
nvaddi891-art <nvaddi891@gmail.com>
2026-05-16 16:39:06 +0530

    installed axios

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000/api',
+   baseURL: 'https://contact-manager-backend-vrhd.onrender.com/api',
 });
 
 export default API;
